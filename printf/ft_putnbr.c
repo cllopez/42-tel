36ca9ec71262d42e47a57424bdb9ef29bec02b43
Claudio Lopez <claudiolopero@gmail.com>
2025-01-30 13:56:01 +0100

    printf

--- printf/ft_putnbr.c
+++ printf/ft_putnbr.c
@@ -6,25 +6,91 @@
 /*   By: cllopez- <cllopez-@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 14:53:02 by cllopez-          #+#    #+#             */
-/*   Updated: 2025/01/30 11:48:39 by cllopez-         ###   ########.fr       */
+/*   Updated: 2025/01/30 13:51:40 by cllopez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_putnbr(va_list arg_ptr, int base, char c)
+static int	ft_int(va_list a)
 {
+	int total;
 	int nb;
+
+	total = 0;
+	nb = va_arg(a, int);
+	if (nb == 0)
+		return (1);
+	else if (nb < 0)
+	{
+		nb *= -1;
+		total++;
+		if	(nb == -2147483648)
+			return (11);
+	}	
+	else if (nb > 0)
+	{
+		total++;
+		nb /= 10;
+	}
+	return (total);
+}
+
+static int	ft_aux_uint(va_list a)
+{
+	int				total;
+	unsigned int	nb;
+
+	total = 0;
+	nb = va_arg(a, unsigned int);
+	ft_putnbr_base(nb, "0123456789", 10);
+	if (nb == 0)
+		return (1);
+	while (nb > 0)
+	{
+		total++;
+		nb = nb / 10;
+	}
+	return (total);
+}
+
+void	ft_wnbr_base(unsigned long num, char *base, int base_len)
+{
+	if (num >= (unsigned long)base_len)
+		ft_putnbr_base(num / base_len, base, base_len);
+	write(1, &base[num % base_len], 1);
+}
+
+static int	ft_hexbase(char c, va_list a)
+{
+	int	total;
+	unsigned int	nb;
+
+	total = 0;
+	nb = va_arg(a, unsigned int);
+	if	(c == 'x')
+		ft_wnbr_base(nb, "0123456789abcdef", 16);
+	if	(c == 'X')
+		ft_wnbr_base(nb, "0123456789ABCDEF", 16);
+	if	(nb < 0)
+		nb = -nb;
+	else if (nb == 0)
+		return (1);
+	while (nb > 0)
+	{
+		total++;
+		nb /= 16;
+	}
+	return (total);
+}
+
+int ft_type(char c, va_list a)
+{
 	int total;
 
-	nb = va_arg(arg_ptr, int);
 	total = 0;
-	if (c == 'u')
-	{
-		nb = (unsigned int)nb;
-		total += ft_putnbrbase(nb, base, c);
-	}
-	else
-		total += ft_putnbrbase(nb, base, c);
-	return(total);
+	if (c == 'i' || c == 'd')
+		total += ft_int(a);
+	else if	(c == 'u')
+		total += ft_uint(a);
 }
--- printf/test.c
+++ printf/test.c
@@ -1,4 +1,5 @@
 #include "ft_printf.h"
+#include <limits.h>
 
 int main() {
 	// Test du format %c
@@ -14,9 +15,13 @@ int main() {
 	printf(" %p %p \n\n", NULL, (void *)&nombre);
 	ft_printf(" %p %p \n", NULL, (void *)&nombre); */
 	// Test du format %d
-	int entier_decimal = -1;
-	printf(" %d \n\n", entier_decimal);
-	ft_printf(" %d \n", entier_decimal);
+	/* int entier_decimal = -42; */
+	printf(" %d %d %ld %ld %lu %d %d", INT_MAX, INT_MIN, LONG_MAX, LONG_MIN, ULONG_MAX, 0, 
+	-42);
+	ft_printf(" %d %d %d %d %d %d %d", INT_MAX, INT_MIN, LONG_MAX, LONG_MIN, ULONG_MAX, 0, 
+	-42);
+	/* printf(" %d \n\n", entier_decimal);
+	ft_printf(" %d \n", entier_decimal); */
 	// Test du format %i
 	int entier_base10 = 67890;
 	printf(" %i \n\n", entier_base10);
